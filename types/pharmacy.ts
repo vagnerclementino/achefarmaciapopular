7ecccf68f079c340c8907068521f6584a9f8126d
Vagner Clementino <76048847+vagnerclementino-hotmart@users.noreply.github.com>
2025-04-25 18:32:52 -0300

    Setup (#1)
    * initial setup
    
    * feat: Implement pharmacy filtering and update CSV structure with types

---
+++ types/pharmacy.ts
@@ -0,0 +1,10 @@
+export interface Pharmacy {
+  state: string;
+  cod: string;
+  city: string;
+  CNPJ: string;
+  name: string;
+  address: string;
+  neighborhood: string;
+  credential_date: string;
+}
